fa6bae98c52775386c2d94024d171f4f50dec3ec
Vitoria Merlino <vitoriadacostamouramerlino@gmail.com>
2026-08-18 01:01:10 -0300

    feat: adiciona o loop com time.sleep para os lembretes

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import time
 from plyer import notification
 
-notification.notify(
-    title='Pausa para alguns goles!',
-    message='De gole em gole a pele fica hidratada!',
-    app_name='Água!',
-    timeout=10  # Duration in seconds
-)
+INTERVALO_SEGUNDOS = 3600
+print("Água! iniciado com sucesso. Você receberá uma notificação a cada hora para se lembrar de beber água.")
+
+while True:
+    time.sleep(INTERVALO_SEGUNDOS)
+
+    notification.notify(
+        title='Pausa para alguns goles!',
+        message='De gole em gole a pele fica hidratada!',
+        app_name='Água!',
+        timeout=10  # Duration in seconds
+    )
